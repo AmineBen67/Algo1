bcd1b974094c6d892435521b97da60b6741996ce
AmineBen67 <benchekamine67@gmail.com>
2023-11-15 09:22:21 +0100

    ajout d'espaces lisibilité

--- Algo2/Exo8.php
+++ Algo2/Exo8.php
@@ -2,7 +2,7 @@
 $url="https://www.modernhoney.com/wp-content/uploads/2019/12/One-Bowl-Chocolate-Chip-Cookie-Recipe-5-scaled.jpg";
 $nbeRepetitions=4;
 function repeterImage($url,$nbeRepetitions){
-    for($j=0;$j<$nbeRepetitions;$j++) {
+    for($j=0 ; $j < $nbeRepetitions ; $j++) {
         echo"<img src='$url'>";
     }
 }
